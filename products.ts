af678125639534149b1ef0f03969f8888c82c7a7
Justinsuen0917 <132532622+Justinsuen0917@users.noreply.github.com>
2025-03-25 03:47:24 -0700

    Create products.ts

---
+++ products.ts
@@ -0,0 +1,7 @@
+export const products = [
+    { id: 1, name: "Product 1", price: 10, image: "image1.jpg" },
+    { id: 2, name: "Product 2", price: 20, image: "image2.jpg" },
+    { id: 3, name: "Product 3", price: 30, image: "image3.jpg" },
+    { id: 4, name: "Product 4", price: 40, image: "image4.jpg" },
+    { id: 5, name: "Product 5", price: 50, image: "image5.jpg" },
+];
